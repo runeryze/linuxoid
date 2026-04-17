2d4318cc3462977598b03b14addb5bcd6f4b8fa9
runeryze <k3nt123456@gmail.com>
2026-04-17 23:44:48 +1000

    firstLab directory added

---
+++ firstLab/twoSum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums, target):
+        seen = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in seen:
+                return [seen[complement], i]
+            seen[num] = i
+        return []
+
+solution = Solution()
+
+nums = [2, 7, 11, 15]
+target = 9
+
+print(solution.twoSum(nums, target))
